ff1439cfce70d8ade46c5209175f9e2e98e5bf51
Samuel <Genesis.samuel@gmail.com>
2011-05-02 23:38:15 +0100

    micropost adicionado

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110502191342) do
+ActiveRecord::Schema.define(:version => 20110502223524) do
+
+  create_table "microposts", :force => true do |t|
+    t.string   "content"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "microsposts", :force => true do |t|
     t.string   "content"
